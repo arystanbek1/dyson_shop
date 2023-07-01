9be9dc8e9ad68126669f1d61bfd26a521dd378e0
arystanbekabdrahmanov <president_abd@bk.ru>
2023-07-01 16:09:12 +0600

    first commit

---
+++ main/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+#- [ ] Class DysonModel - Картинка, Название,
+# Артикул, Прайс, есть в наличии или нету, страна Производства,
+# Характеристика(цвет, кол насадок), добавить в корзину,
+class DysonModel(models.Model):
+    pass
+
+
+
